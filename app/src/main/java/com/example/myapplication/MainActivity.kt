8f7dabd8417bd1193a3e998f190b73e2aa207392
IW_Dnyaneshwar <dnyaneshwar@iwizardsolutions.com>
2021-05-26 14:54:30 +0530

    Fastlane files

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.lang.IllegalArgumentException
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.crashlytics.CustomKeysAndValues
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val keysAndValues = CustomKeysAndValues.Builder()
+            .putString("string key", "Test")
+            .putString("string key 2", "string  value 2")
+            .putBoolean("boolean key", true)
+            .putBoolean("boolean key 2", false)
+            .putFloat("float key", 1.01f)
+            .putFloat("float key 2", 2.02f)
+            .build()
+
+        FirebaseCrashlytics.getInstance().setCustomKeys(keysAndValues)
+        FirebaseCrashlytics.getInstance().setUserId("12345")
+
 //        crash()
+//        try {
+//            crash()
+//        } catch (e: Exception) {
+//            FirebaseCrashlytics.getInstance().recordException(e)
+//            // ...handle the exception.
+//        }
     }
 
-    fun crash(){
-        crash()
+    private fun crash(){
+        throw IllegalAccessException("Crash")
     }
 }
